15d2c3af0dd22b682555b3bd0049dc72debeaceb
RanjitKumarTharu <wangduranjeet@gmail.com>
2026-05-09 15:15:56 +0545

    added home page

--- app/main.py
+++ app/main.py
@@ -3,15 +3,31 @@ from app.schema import Cardio
 from app.model import load_model_scaler
 import pandas as pd
 
+#for html
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+# Templates folder
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    return templates.TemplateResponse(
+        "index.html",
+        {"request": request}
+    )
+
 # object of FastAPI
 
 app = FastAPI()
 model , scaler = load_model_scaler()
 
 # get method 
-@app.get("/")
-def home():
-    return "Welcome to FastAPI Application"
+# @app.get("/")
+# def home():
+#     return "Welcome to FastAPI Application"
 
 # post method for prediction
 
